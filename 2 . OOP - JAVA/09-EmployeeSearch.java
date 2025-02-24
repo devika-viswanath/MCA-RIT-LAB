037a2852e8ed72daa018ef237758a9c5a6bf0c95
Devika.B <24mp2265@rit.ac.in>
2025-02-24 13:00:43 +0000

    done

--- 2 . OOP - JAVA/09-EmployeeSearch.java
+++ 2 . OOP - JAVA/09-EmployeeSearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringSearch {
+public class EmployeeSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
