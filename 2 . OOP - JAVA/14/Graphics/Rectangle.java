b276e91e91be85035daa398918e767e6a2b11eb7
Devika.B <24mp2265@rit.ac.in>
2025-03-03 14:38:17 +0530

    Create Rectangle.java
    Rectangle.java is added

---
+++ 2 . OOP - JAVA/14/Graphics/Rectangle.java
@@ -0,0 +1,16 @@
+package graphics;
+
+// Rectangle class implementing Shape interface
+public class Rectangle implements Shape {
+    double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
