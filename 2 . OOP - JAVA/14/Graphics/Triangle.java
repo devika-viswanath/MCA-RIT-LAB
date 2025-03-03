78ce2c18bc67e8cb3c87382c8c15f1cd7880cca7
Devika.B <24mp2265@rit.ac.in>
2025-03-03 14:47:43 +0530

    Create Triangle.java

---
+++ 2 . OOP - JAVA/14/Graphics/Triangle.java
@@ -0,0 +1,16 @@
+package graphics;
+
+// Triangle class implementing Shape interface
+public class Triangle implements Shape {
+    double base, height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
