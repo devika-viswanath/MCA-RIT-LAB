25d935de39b393221d810885dd5a4aea94b3e2af
Devika.B <24mp2265@rit.ac.in>
2025-04-06 00:14:39 +0530

    Update 10-StringManipulations.java

--- 2 . OOP - JAVA/10-StringManipulations.java
+++ 2 . OOP - JAVA/10-StringManipulations.java
@@ -1,3 +1,4 @@
+//method1
 import java.util.Scanner;
 
 public class StringManipulations {
@@ -41,5 +42,42 @@ public class StringManipulations {
         System.out.println("Reversed string: " + reversed);
 
         scanner.close();
+    }
+}
+
+//method 2
+import java.util.Scanner;
+public class StringManipulation{
+    public static void main(String arg[]){
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str=obj.nextLine();
+        System.out.println("length of the string:"+str.length());
+        System.out.println("uppercase:"+str.toUpperCase());
+        System.out.println("lowecase:"+str.toLowerCase());
+        System.out.println("Enter the index to extract the string:");
+        int index=obj.nextInt();
+        obj.nextLine();
+        if(index>=0 && index<str.length()){
+            System.out.println("extract character:"+ str.charAt(index));
+
+        }
+        else{
+            System.out.println("invalid index");
+        }
+        System.out.println("Enter the Start index:");
+        int start=obj.nextInt();
+        obj.nextLine();
+        System.out.println("Enter the end index:");
+        int end=obj.nextInt();
+        obj.nextLine();
+        if(start>=0 && end<str.length() && start<end){
+            System.out.println("Substring:"+str.substring(start,end));
+        }
+        else{
+            System.out.println("index invalid");
+        }
+        String reversed=new StringBuilder(str).reverse().toString();
+        System.out.println("reversed string:"+reversed);
     }
 }
