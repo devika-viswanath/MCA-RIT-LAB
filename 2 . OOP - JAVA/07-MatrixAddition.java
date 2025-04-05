165cf6a157c3cbd95f0af331aa7d33a8e9d17a84
Devika.B <24mp2265@rit.ac.in>
2025-04-05 16:16:46 +0530

    Update 07-MatrixAddition.java

--- 2 . OOP - JAVA/07-MatrixAddition.java
+++ 2 . OOP - JAVA/07-MatrixAddition.java
@@ -1,3 +1,4 @@
+//method 1
 import java.util.Scanner;
 
 class Matrix {
@@ -60,5 +61,68 @@ public class MatrixAddition {
         sumMatrix.display();
 
         scanner.close();
+    }
+}
+
+//method 2
+
+import java.util.Scanner;
+class Matrix{
+    int rows;
+    int columns;
+    int[][] data;
+    Matrix(int rows,int columns){
+        this.rows=rows;
+        this.columns=columns;
+        data=new int[rows][columns];
+    }
+    Matrix fillmatrix(Scanner obj){
+       // System.out.println("enter the elements in the matrix:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                data[i][j]=obj.nextInt();
+            }
+        }
+        return this;
+    }
+    Matrix add( Matrix other){
+        Matrix result=new Matrix(rows,columns);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                result.data[i][j]=this.data[i][j]+other.data[i][j];
+
+            }
+            
+        }
+        return result;
+    }
+    void display(){
+        System.out.println("result:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(data[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+public class MatrixAddition{
+    public static void main(String arg[]){
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows=obj.nextInt();
+        
+        System.out.println("Enter the number of columns:");
+        int columns=obj.nextInt();
+        
+        Matrix matrix1=new Matrix(rows,columns);
+        Matrix matrix2=new Matrix(rows,columns);
+        System.out.println("enter the elements in the matrix1:");
+        matrix1.fillmatrix(obj);
+        System.out.println("enter the elements in the matrix2");
+        matrix2.fillmatrix(obj);
+        Matrix sum=matrix1.add(matrix2);
+        sum.display();
     }
 }
