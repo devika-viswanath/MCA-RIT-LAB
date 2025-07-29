b481187d4de07c11cd5b3b8893b3c9d41e9b27e6
DEVIKA B <183391414+devika-viswanath@users.noreply.github.com>
2025-07-29 13:43:46 +0530

    Create MainActivity.java

---
+++ 3 . MAD LAB/Cycle - 2/Exp1_DisplayDateTime/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.datetimedisplayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnShowDateTime;
+    TextView txtDate, txtTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowDateTime = findViewById(R.id.btnShowDateTime);
+        txtDate = findViewById(R.id.txtDate);
+        txtTime = findViewById(R.id.txtTime);
+
+        btnShowDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date currentDate = new Date();
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+
+                txtDate.setText("Date: " + dateFormat.format(currentDate));
+                txtTime.setText("Time: " + timeFormat.format(currentDate));
+            }
+        });
+    }
+}
