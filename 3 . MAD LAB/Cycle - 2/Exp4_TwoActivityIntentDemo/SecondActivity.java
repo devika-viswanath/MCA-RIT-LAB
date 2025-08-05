e42af1031a6bf8452a5a03bd4b89765ec10aea3b
DEVIKA B <183391414+devika-viswanath@users.noreply.github.com>
2025-08-05 14:15:43 +0530

    Create SecondActivity.java

---
+++ 3 . MAD LAB/Cycle - 2/Exp4_TwoActivityIntentDemo/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.intentexample;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.textView);
+
+        String message = getIntent().getStringExtra("message");
+        textView.setText(message);
+    }
+}
