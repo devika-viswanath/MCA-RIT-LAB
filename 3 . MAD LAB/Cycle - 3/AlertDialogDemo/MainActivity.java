3c7ae394e853b41faa86797c03fedaa3530ed62a
DEVIKA B <183391414+devika-viswanath@users.noreply.github.com>
2025-11-01 13:39:44 +0530

    Refactor AlertDialog implementation in MainActivity

--- 3 . MAD LAB/Cycle - 3/AlertDialogDemo/MainActivity.java
+++ 3 . MAD LAB/Cycle - 3/AlertDialogDemo/MainActivity.java
@@ -1,45 +1,59 @@
-package com.example.alertdialogdemo;
+package com.example.myalert;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAlert = findViewById(R.id.btnAlert);
+        Button showDialogButton = findViewById(R.id.show_dialog_button);
 
-        btnAlert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        showDialogButton.setOnClickListener(v -> {
+//            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//            builder.setTitle("Alert Dialog Title")
+//                    .setMessage("This is an alert dialog message.")
+//                    .setCancelable(false)
+//                    .setPositiveButton("Yes", (dialog, which) ->
+//                            Toast.makeText(MainActivity.this, "You clicked YES", Toast.LENGTH_SHORT).show())
+//                    .setNegativeButton("No", (dialog, which) ->
+//                            Toast.makeText(MainActivity.this, "You clicked NO", Toast.LENGTH_SHORT).show())
+//                    .setNeutralButton("Cancel", (dialog, which) ->
+//                            Toast.makeText(MainActivity.this, "You clicked CANCEL", Toast.LENGTH_SHORT).show());
+//            AlertDialog alertDialog = builder.create();
+//            alertDialog.show();
+//        });
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Alert Dialog")
-                        .setMessage("Are you sure you want to proceed?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Toast.makeText(MainActivity.this, "You clicked Yes", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Toast.makeText(MainActivity.this, "You clicked No", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+//        you dont need toast message
 
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
+
+        showDialogButton.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Alert Dialog Title")
+                    .setMessage("This is an alert dialog message.")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        // Action when YES is clicked
+                    })
+                    .setNegativeButton("No", (dialog, which) -> {
+                        // Action when NO is clicked
+                        dialog.cancel();
+                    })
+                    .setNeutralButton("Cancel", (dialog, which) -> {
+                        // Action when CANCEL is clicked
+                        dialog.dismiss();
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         });
+
+
     }
 }
