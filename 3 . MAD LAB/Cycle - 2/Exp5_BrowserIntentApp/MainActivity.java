b9a84028a688ea019c360d97d5dea0809e679ad9
DEVIKA B <183391414+devika-viswanath@users.noreply.github.com>
2025-11-02 15:53:23 +0530

    Implement button to open browser with Google URL
    Added functionality to open a browser with a specified URL when the button is clicked.

--- 3 . MAD LAB/Cycle - 2/Exp5_BrowserIntentApp/MainActivity.java
+++ 3 . MAD LAB/Cycle - 2/Exp5_BrowserIntentApp/MainActivity.java
@@ -28,3 +28,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+//other way
+
+package com.example.sharedpref;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //link to xml
+        Button btnOpenBrowser =findViewById(R.id.btnOpenBrowser);
+
+        btnOpenBrowser.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com"));
+            startActivity(intent);
+
+
+        });
+    }
+}
+
