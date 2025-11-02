02da8b3646bf7e9c9be38adc681b8e441ca6519c
DEVIKA B <183391414+devika-viswanath@users.noreply.github.com>
2025-11-02 15:55:40 +0530

    Rename button variable and update click listener

--- 3 . MAD LAB/Cycle - 2/Exp5_BrowserIntentApp/MainActivity.java
+++ 3 . MAD LAB/Cycle - 2/Exp5_BrowserIntentApp/MainActivity.java
@@ -52,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //link to xml
-        Button btnOpenBrowser =findViewById(R.id.btnOpenBrowser);
+        Button buttonOpenBrowser =findViewById(R.id.buttonOpenBrowser);
 
-        btnOpenBrowser.setOnClickListener(v -> {
+        buttonOpenBrowser.setOnClickListener(v -> {
 
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.google.com"));
